7ef4079b77fd762fea63bdbe01f8cc22939f8cbd
Mohit Sharma <mohitms015@gmail.com>
2021-09-20 12:56:54 +0530

    update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `http://cloth-fits.vercel.app/`;
+export const endpoint = `https://cloth-fits.herokuapp.com/`;
 export const prodEndpoint = `fill me in when we deploy`;
 export const perPage = 4;
