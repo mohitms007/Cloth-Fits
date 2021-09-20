deb3f7374cc941ef7b9a0c5ed0f062e61b0d9286
Mohit Sharma <mohitms015@gmail.com>
2021-09-20 13:50:53 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
-export const endpoint = `https://dry-ridge-09994.herokuapp.com/https://cloth-fits.herokuapp.com/`;
-export const prodEndpoint = `https://dry-ridge-09994.herokuapp.com/https://cloth-fits.herokuapp.com/`;
+export const endpoint = `https://cloth-fits.herokuapp.com/`;
+export const prodEndpoint = `https://cloth-fits.herokuapp.com/`;
 export const perPage = 4;
