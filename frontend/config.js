a0f4f093ebd5a489b20b280ef9a816d4103bb638
Mohit Sharma <mohit@attenioun.com>
2022-03-19 20:44:49 +0530

    config change

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `https://cloth-fits.herokuapp.com/api/graphql`;
-export const prodEndpoint = `https://cloth-fits.herokuapp.com/`;
+export const prodEndpoint = `https://cloth-fits.herokuapp.com/api/graphql`;
 export const perPage = 2;
